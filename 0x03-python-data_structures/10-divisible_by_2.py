f1ab1ec206bf5a2c486af39a2f85277c4c3741cd
Farah-halim <level2bis2@gmail.com>
2023-06-13 17:48:21 +0000

    done

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    mylist = []
+    counert = len(my_list)
+    for i in range(counter):
+        if my_list[i] % 2 == 0:
+            mylist.append(True)
+        else:
+            mylist.append(False)
+    return mylist
