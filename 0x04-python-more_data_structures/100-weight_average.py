6a7be85ed7d9280b99d3f1270e22f3deecd960fe
Farah-halim <level2bis2@gmail.com>
2023-06-23 16:43:36 +0000

    DONE

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda n: list(map(lambda x: x * x, n)), matrix))
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    to_del = []
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
-            to_del.append(key)
-    for i in to_del:
-        del a_dictionary[i]
-    return a_dictionary
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+    return (a_dictionary)
